c847caceb11a225a9d7aa7295b7b62d76b6101a5
wh.y <wh.y@navercorp.com>
2018-05-24 18:12:30 +0900

    layout project

---
+++ packages/simple-module-a/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "name": "@why-monorepo-demo/simple-module-a",
+    "version": "1.0.0"
+};
---
+++ packages/simple-module-b/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "name": "@why-monorepo-demo/simple-module-b",
+    "version": "1.0.0"
+};
