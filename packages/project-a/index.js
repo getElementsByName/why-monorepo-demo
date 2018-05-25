c385541e594c30468226789650df599dc6e7d7e9
getElementByName <korywh@gmail.com>
2018-05-26 00:19:34 +0900

    add a peerDependency problem

--- packages/project-a/index.js
+++ packages/project-a/index.js
@@ -3,3 +3,11 @@ const ModuleB = require("@why-monorepo-demo/simple-module-b");
 
 console.log(ModuleA);
 console.log(ModuleB);
+
+try{
+    const PluginA = require("@why-monorepo-demo/simple-plugin-a");
+    
+    console.log(PluginA);
+} catch(e){
+    console.error("Can't load '@why-monorepo-demo/simple-plugin-a'", e);
+}
